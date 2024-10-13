8207ab2fd9aeb57a4662cf77f9dcab713c473638
Tarek Mamdouh <egypty3@gmail.com>
2024-10-14 02:40:02 +0300

    Add project files.

---
+++ Lect1/Data/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lect1.Models;
+
+namespace Lect1.Data
+{
+	public class TodoContext : DbContext
+	{
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+		{
+            
+        }
+	    public DbSet<Lect1.Models.TodoItem> TodoItem { get; set; } = default!;
+    }
+}
